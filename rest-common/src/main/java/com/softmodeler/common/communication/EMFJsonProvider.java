4b89d8b0c1ffa9459518c8360876591bdc8b018e
Flavio Donzé <flavio.donze@scodi.ch>
2020-04-14 11:52:44 +0200

    configure Builder instead of ObjectMapper, for future version 3.x

--- rest-common/src/main/java/com/softmodeler/common/communication/EMFJsonProvider.java
+++ rest-common/src/main/java/com/softmodeler/common/communication/EMFJsonProvider.java
@@ -11,12 +11,13 @@ import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.core.JsonGenerator.Feature;
 import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.core.json.JsonWriteFeature;
 import com.fasterxml.jackson.databind.DatabindContext;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JavaType;
@@ -24,9 +25,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectMapper.DefaultTyping;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.annotation.JsonTypeIdResolver;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
 import com.fasterxml.jackson.databind.jsontype.impl.ClassNameIdResolver;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 import com.fasterxml.jackson.module.paranamer.ParanamerModule;
 import com.softmodeler.common.communication.ser.EMFDeserializers;
@@ -47,7 +52,7 @@ public class EMFJsonProvider extends JacksonJaxbJsonProvider {
 	public static class IdResolver extends ClassNameIdResolver {
 
 		public IdResolver() {
-			super(getTypeFactory().constructSimpleType(Object.class, null), getTypeFactory());
+			super(getTypeFactory().constructSimpleType(Object.class, null), getTypeFactory(), BasicPolymorphicTypeValidator.builder().allowIfSubType(Object.class).build());
 		}
 
 		@Override
@@ -95,30 +100,31 @@ public class EMFJsonProvider extends JacksonJaxbJsonProvider {
 	}
 
 	private ObjectMapper setupMapper() {
-		ObjectMapper mapper = new ObjectMapper();
+		JsonMapper.Builder builder = JsonMapper.builder();
 
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.ENGLISH); // same as emf
 		dateFormat.setTimeZone(TimeZone.getDefault());
-		mapper.setDateFormat(dateFormat);
-		mapper.setTimeZone(TimeZone.getDefault());
-
-		mapper.setSerializationInclusion(Include.NON_NULL);
-		mapper.setDefaultPropertyInclusion(Include.NON_NULL);
-		mapper.setVisibility(PropertyAccessor.ALL, Visibility.NONE);
-		mapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
-		mapper.setVisibility(PropertyAccessor.CREATOR, Visibility.ANY);
-
-		mapper.configure(DeserializationFeature.FAIL_ON_MISSING_CREATOR_PROPERTIES, false);
-		mapper.configure(Feature.ESCAPE_NON_ASCII, true);
-		mapper.configure(SerializationFeature.WRAP_EXCEPTIONS, true);
-		mapper.configure(DeserializationFeature.WRAP_EXCEPTIONS, true);
-		mapper.configure(SerializationFeature.WRITE_ENUMS_USING_TO_STRING, true);
-		mapper.configure(DeserializationFeature.READ_ENUMS_USING_TO_STRING, true);
+		builder.defaultDateFormat(dateFormat);
+		builder.defaultTimeZone(TimeZone.getDefault());
 		
-		mapper.enableDefaultTyping(DefaultTyping.NON_FINAL);
-		mapper.setTypeFactory(getTypeFactory());
-		
-		mapper.addMixIn(Object.class, ObjectMixing.class);
+		builder.serializationInclusion(Include.NON_NULL);
+		builder.defaultPropertyInclusion(JsonInclude.Value.construct(Include.NON_NULL, Include.NON_NULL));
+		builder.visibility(PropertyAccessor.ALL, Visibility.NONE);
+		builder.visibility(PropertyAccessor.FIELD, Visibility.ANY);
+		builder.visibility(PropertyAccessor.CREATOR, Visibility.ANY);
+
+		builder.configure(DeserializationFeature.FAIL_ON_MISSING_CREATOR_PROPERTIES, false);
+		builder.configure(JsonWriteFeature.ESCAPE_NON_ASCII, true);
+		builder.configure(SerializationFeature.WRAP_EXCEPTIONS, true);
+		builder.configure(DeserializationFeature.WRAP_EXCEPTIONS, true);
+		builder.configure(SerializationFeature.WRITE_ENUMS_USING_TO_STRING, true);
+		builder.configure(DeserializationFeature.READ_ENUMS_USING_TO_STRING, true);
+
+		PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder().allowIfSubType(Object.class).build();
+		builder.activateDefaultTyping(ptv, DefaultTyping.NON_FINAL);
+		builder.typeFactory(getTypeFactory());		
+
+		builder.addMixIn(Object.class, ObjectMixing.class);
 
 		SimpleModule module = new SimpleModule("softmodeler", Version.unknownVersion()) {
 			private static final long serialVersionUID = 2890805560707096019L;
@@ -132,8 +138,11 @@ public class EMFJsonProvider extends JacksonJaxbJsonProvider {
 				context.addSerializers(serializers);
 			}
 		};
-		mapper.registerModule(module);
-		mapper.registerModule(new ParanamerModule());
+		builder.addModule(module);
+		builder.addModule(new ParanamerModule());
+		builder.addModule(new Jdk8Module());
+
+		JsonMapper mapper = builder.build();
 		return mapper;
 	}
 }
