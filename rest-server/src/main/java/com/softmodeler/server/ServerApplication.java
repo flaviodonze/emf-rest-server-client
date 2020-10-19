cca9ffe05cfd17ac8b22a82518b77cbb4e55efcb
Flavio Donzé <flavio.donze@scodi.ch>
2020-10-19 10:01:07 +0200

    added plain transfer data logger

--- rest-server/src/main/java/com/softmodeler/server/ServerApplication.java
+++ rest-server/src/main/java/com/softmodeler/server/ServerApplication.java
@@ -1,12 +1,18 @@
 package com.softmodeler.server;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.cxf.Bus;
+import org.apache.cxf.common.util.StringUtils;
 import org.apache.cxf.endpoint.Server;
 import org.apache.cxf.ext.logging.LoggingFeature;
+import org.apache.cxf.ext.logging.event.LogEvent;
+import org.apache.cxf.ext.logging.event.PrintWriterEventSender;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
 import org.apache.cxf.jaxrs.openapi.OpenApiFeature;
 import org.apache.cxf.jaxrs.swagger.ui.SwaggerUiConfig;
@@ -30,7 +36,7 @@ public class ServerApplication {
 	}
 
 	@Bean
-	public Server rsServer() {
+	public Server rsServer() throws Exception {
 		List<Object> providers = new ArrayList<>(CommunicationUtil.getProviders());
 		providers.add(new ServerExceptionMapper());
 		
@@ -40,10 +46,67 @@ public class ServerApplication {
 		endpoint.setServiceBeans(Arrays.<Object>asList(new POJOServiceImpl(), new NotificationDefinitionServiceImpl(),
 				new TreeNodeServiceImpl()));
 		endpoint.setAddress("/");
-		endpoint.setFeatures(Arrays.asList(createOpenApiFeature(), new LoggingFeature()));
+//		endpoint.setFeatures(Arrays.asList(createOpenApiFeature(), new LoggingFeature()));
+		endpoint.setFeatures(Arrays.asList(loggingFeature()));
 		return endpoint.create();
 	}
 
+	private LoggingFeature loggingFeature() throws FileNotFoundException {
+//		return new LoggingFeature();
+
+		// logger which is focused on comparing transfer data e.g. after an update of framework
+		LoggingFeature loggingFeature = new LoggingFeature();
+		PrintWriter writer = new PrintWriter("d:/temp/server.txt");
+		loggingFeature.setSender(new PrintWriterEventSender(writer) {
+		    /** {@inheritDoc}*/
+		    @Override
+		    public void send(LogEvent event) {
+		        synchronized (writer) {
+//		            writer.print(Instant.now().toString() + " - PrintWriterEventSender - ");
+		            writer.print(format(event));
+		            writer.flush();
+		        }
+		    }
+		    
+		    public String format(LogEvent event) {
+		        StringBuilder b = new StringBuilder();
+		        // Start from the next line to have the output well-aligned
+		        b.append(event.getType()).append('\n');
+		        
+		        write(b, "Address", event.getAddress());
+		        write(b, "HttpMethod", event.getHttpMethod());
+		        write(b, "Content-Type", event.getContentType());
+		        write(b, "ResponseCode", event.getResponseCode());
+//		        write(b, "ExchangeId", event.getExchangeId());
+		        if (event.getServiceName() != null) {
+//		            write(b, "ServiceName", localPart(event.getServiceName()));
+//		            write(b, "PortName", localPart(event.getPortName()));
+//		            write(b, "PortTypeName", localPart(event.getPortTypeName()));
+		        }
+		        if (event.getFullContentFile() != null) {
+		            write(b, "FullContentFile", event.getFullContentFile().getAbsolutePath());
+		        }
+
+		        Map<String, String> headers = event.getHeaders();
+		        headers.remove("Date");
+
+				write(b, "Headers", headers.toString());
+		        if (!StringUtils.isEmpty(event.getPayload())) {
+		            write(b, "Payload", event.getPayload());
+		        }
+		        return b.toString();
+		    }
+
+		    protected void write(StringBuilder b, String key, String value) {
+		        if (value != null) {
+		            b.append("    ").append(key).append(": ").append(value).append('\n');
+		        }
+		    }
+		    
+		});
+		return loggingFeature;
+	}
+	
 	@Bean
 	public OpenApiFeature createOpenApiFeature() {
 		final OpenApiFeature openApiFeature = new OpenApiFeature();
